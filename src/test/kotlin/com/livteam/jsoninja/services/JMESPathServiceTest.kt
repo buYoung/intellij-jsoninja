fb4534040f0e55d3e0abdaf592c524794642dd2b
BuYoungLee <leebu18@gmail.com>
2025-11-20 17:17:30 +0900

    chore: 테스트코드 오류수정

--- src/test/kotlin/com/livteam/jsoninja/services/JMESPathServiceTest.kt
+++ src/test/kotlin/com/livteam/jsoninja/services/JMESPathServiceTest.kt
@@ -1,41 +1,39 @@
 package com.livteam.jsoninja.services
 
+import com.intellij.openapi.components.service
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.livteam.jsoninja.services.JMESPathService
+import com.livteam.jsoninja.model.JsonQueryType
+import com.livteam.jsoninja.settings.JsoninjaSettingsState
 
 class JMESPathServiceTest : BasePlatformTestCase() {
-    private lateinit var jmesPathService: JMESPathService
+    private lateinit var jsonQueryService: JsonQueryService
 
     override fun setUp() {
         super.setUp()
-        jmesPathService = JMESPathService()
+        jsonQueryService = project.service()
+        JsoninjaSettingsState.getInstance(project).jsonQueryType = JsonQueryType.JMESPATH.name
     }
 
     fun testQuery() {
-        // Test with valid JSON and valid JsonPath expression
         val jsonString = """{"name": "John", "age": 30, "address": {"city": "New York", "country": "USA"}}"""
-        val jsonPathExpression = "$.address.city"
-        val result = jmesPathService.query(jsonString, jsonPathExpression)
+        val jmesPathExpression = "address.city"
+        val result = jsonQueryService.query(jsonString, jmesPathExpression)
         assertEquals("\"New York\"", result)
 
-        // Test with valid JSON and invalid JsonPath expression
-        val invalidExpression = "$.invalid.path"
-        val nullResult = jmesPathService.query(jsonString, invalidExpression)
+        val invalidExpression = "address["
+        val nullResult = jsonQueryService.query(jsonString, invalidExpression)
         assertNull(nullResult)
 
-        // Test with invalid JSON
         val invalidJson = "{invalid json}"
-        val errorResult = jmesPathService.query(invalidJson, jsonPathExpression)
+        val errorResult = jsonQueryService.query(invalidJson, jmesPathExpression)
         assertNull(errorResult)
     }
 
     fun testIsValidExpression() {
-        // Test with valid JsonPath expression
-        val validExpression = "$.name"
-        assertTrue(jmesPathService.isValidExpression(validExpression))
+        val validExpression = "name"
+        assertTrue(jsonQueryService.isValidExpression(validExpression))
 
-        // Test with invalid JsonPath expression
-        val invalidExpression = "$.[invalid"
-        assertFalse(jmesPathService.isValidExpression(invalidExpression))
+        val invalidExpression = "address["
+        assertFalse(jsonQueryService.isValidExpression(invalidExpression))
     }
 }
