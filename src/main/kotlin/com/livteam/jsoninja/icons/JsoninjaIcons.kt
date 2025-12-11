ace3dbfb4759b5e4cbddb585afd1a4dd4a2af1f8
buyong <leebu18@gmail.com>
2025-12-12 06:44:57 +0900

    fix: unuse jetbrains internalapi

--- src/main/kotlin/com/livteam/jsoninja/icons/JsoninjaIcons.kt
+++ src/main/kotlin/com/livteam/jsoninja/icons/JsoninjaIcons.kt
@@ -1,65 +1,59 @@
 package com.livteam.jsoninja.icons
 
-import com.intellij.ui.IconManager
 import com.intellij.openapi.project.Project
-import com.livteam.jsoninja.settings.JsoninjaSettingsState
+import com.intellij.openapi.util.IconLoader.getIcon
 import com.livteam.jsoninja.model.JsonIconPack
+import com.livteam.jsoninja.settings.JsoninjaSettingsState
 import javax.swing.Icon
 
 
 object JsoninjaIcons {
-    private fun load(path: String, cacheKey: Int, flags: Int): Icon {
-        return IconManager.getInstance()
-            .loadRasterizedIcon(path, JsoninjaIcons::class.java.classLoader, cacheKey, flags)
-    }
-
-    private fun load(expUIPath: String, path: String, cacheKey: Int, flags: Int): Icon {
-        return IconManager.getInstance()
-            .loadRasterizedIcon(path, expUIPath, JsoninjaIcons::class.java.classLoader, cacheKey, flags)
+    private fun load(path: String): Icon {
+        return getIcon(path, javaClass)
     }
 
     @JvmField
-    val ToolWindowIcon: Icon = load("icons/expui/toolWindowIcon-20.svg", "icons/classic/toolWindowIcon-16.svg", 0, 2)
+    val ToolWindowIcon: Icon = load("/icons/classic/toolWindowIcon-16.svg")
 
     @JvmField
-    val PrettyIcon: Icon = load("icons/expui/prettyIcon-20.svg", "icons/classic/prettyIcon-16.svg", 423984, 2)
+    val PrettyIcon: Icon = load("/icons/classic/prettyIcon-16.svg")
 
     @JvmField
-    val UglyIcon: Icon = load("icons/expui/uglyIcon-20.svg", "icons/classic/uglyIcon-16.svg", 912837, 2)
+    val UglyIcon: Icon = load("/icons/classic/uglyIcon-16.svg")
 
     @JvmField
-    val EscapeIcon: Icon = load("icons/expui/escapeIcon-20.svg", "icons/classic/escapeIcon-16.svg", 283746, 2)
+    val EscapeIcon: Icon = load("/icons/classic/escapeIcon-16.svg")
 
     @JvmField
-    val UnescapeIcon: Icon = load("icons/expui/unescapeIcon-20.svg", "icons/classic/unescapeIcon-16.svg", 572819, 2)
+    val UnescapeIcon: Icon = load("/icons/classic/unescapeIcon-16.svg")
 
     @JvmField
-    val GenerateIcon: Icon = load("icons/expui/generateIcon-20.svg", "icons/classic/generateIcon-16.svg", 384726, 2)
+    val GenerateIcon: Icon = load("/icons/classic/generateIcon-16.svg")
 
     @JvmField
-    val DiffIcon: Icon = load("icons/expui/diffIcon-20.svg", "icons/classic/diffIcon-16.svg", 192837, 2)
+    val DiffIcon: Icon = load("/icons/classic/diffIcon-16.svg")
 
     @JvmField
     val PrettyIconV2: Icon =
-        load("icons/expui/v2/prettyIcon-v2-20.svg", "icons/classic/v2/prettyIcon-v2-16.svg", 657483, 2)
+        load("/icons/classic/v2/prettyIcon-v2-16.svg")
 
     @JvmField
-    val UglyIconV2: Icon = load("icons/expui/v2/uglyIcon-v2-20.svg", "icons/classic/v2/uglyIcon-v2-16.svg", 293847, 2)
+    val UglyIconV2: Icon = load("/icons/classic/v2/uglyIcon-v2-16.svg")
 
     @JvmField
     val EscapeIconV2: Icon =
-        load("icons/expui/v2/escapeIcon-v2-20.svg", "icons/classic/v2/escapeIcon-v2-16.svg", 847362, 2)
+        load("/icons/classic/v2/escapeIcon-v2-16.svg")
 
     @JvmField
     val UnescapeIconV2: Icon =
-        load("icons/expui/v2/unescapeIcon-v2-20.svg", "icons/classic/v2/unescapeIcon-v2-16.svg", 472839, 2)
+        load("/icons/classic/v2/unescapeIcon-v2-16.svg")
 
     @JvmField
     val GenerateIconV2: Icon =
-        load("icons/expui/v2/generateIcon-v2-20.svg", "icons/classic/v2/generateIcon-v2-16.svg", 564738, 2)
+        load("/icons/classic/v2/generateIcon-v2-16.svg")
 
     @JvmField
-    val DiffIconV2: Icon = load("icons/expui/v2/diffIcon-v2-20.svg", "icons/classic/v2/diffIcon-v2-16.svg", -1766843, 2)
+    val DiffIconV2: Icon = load("/icons/classic/v2/diffIcon-v2-16.svg")
 
     fun getPrettyIcon(project: Project?): Icon = getIcon(project, PrettyIcon, PrettyIconV2)
     fun getUglifyIcon(project: Project?): Icon = getIcon(project, UglyIcon, UglyIconV2)
